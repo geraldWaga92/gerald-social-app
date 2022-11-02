fbc9f7430dceaff9f375a3a23accfaf5725dfeca
geraldWaga92 <106008160+geraldWaga92@users.noreply.github.com>
2022-11-02 08:12:44 -0800

    install security dependcies

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,7 +1,7 @@
 import { db } from "../connectMysql.js";
 import bcrypt from 'bcryptjs';
 
-//here we will create our user
+//------here we will create our user-----
 export const register = (req, res) => {
     //----check if user exist----
     const q = 'SELECT * FROM users WHERE username = ?'
@@ -9,7 +9,7 @@ export const register = (req, res) => {
     //the '[req.body.username]' is the value the username will hold when a user is registered for the login
     db.query(q, [req.body.username], (err, data) => {
         if(err) return res.status(500).json(err)
-        //this will check if there is already a user
+        //this will check if there is already a user, below there is already a user
         if(data.length) return res.status(409).json('user already exist')
 
         //----Create a user if no User found----
@@ -28,6 +28,7 @@ export const register = (req, res) => {
             req.body.name
         ]
 
+        //this line will create a user in our database
         db.query(q, [values], (err, data) => {
             if(err) return res.status(500).json(err)
             return res.status(200).json('user has been created')
@@ -36,8 +37,28 @@ export const register = (req, res) => {
 
 }
 
+
+
+// --- login function -----
 export const login = (req, res) => {
+
+    // --- Check if there is an error ---
+
+    const q = 'SELECT * FROM users WHERE username = ?'
+
+    //remember that data here returns an array of user
+    db.query(q, [req.body.username], (err, data) => {
+            if(err) return res.status(500).json(err)
+    //the "data.length === 0" means no user, so below checks if there is no user
+          if(data.length === 0)  return res.status(404).json('User not found')
+         })
     
+    // --- And if there NO error ---
+    //here our checkPassword compare the password we set in our database, so we put the "req.body.password" from our database
+    //and remeber that data returns an array so since we only have 1 users we put data[0] as first array followed by the users password
+    const checkPassword = bcrypt.compareSync(req.body.password, data[0].password)
+
+    if(!checkPassword) return res.status(400).json('Wrong password or username')
 }
 
 export const logout = (req, res) => {
