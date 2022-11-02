ab1512a1be8381d578defb0967f278d251b9c83d
geraldWaga92 <106008160+geraldWaga92@users.noreply.github.com>
2022-11-01 19:49:14 -0800

    update index.js with routes endpoint

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express  from "express";
 const app = express();
+import usersRoutes from './routes/users.js';
+import postsRoutes from './routes/posts.js';
+import likesRoutes from './routes/likes.js';
+import commentsRoutes from './routes/comments.js';
+import authRoutes from './routes/auth.js';
 
+
+//this all our endpoint in routes
+app.use('/api/users', usersRoutes)
+app.use('/api/posts', postsRoutes)
+app.use('/api/likes', likesRoutes)
+app.use('/api/comments', commentsRoutes)
+app.use('/api/auth', authRoutes)
 
 
 app.listen(5000, () => {
