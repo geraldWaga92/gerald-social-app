b4f5add57295e72003cbbe93d014597d97d2a9a9
geraldWaga92 <106008160+geraldWaga92@users.noreply.github.com>
2022-11-01 20:41:35 -0800

    install bcryptjs and update auth.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,7 +3,10 @@ import { } from "../controllers/auth.js";
 
 const router = express.Router()
 
-router.get("",)
+//endpoint in auth routes will be connected from our controllers auth.js
+router.get('/register', register)
+router.get('/login', login)
+router.get('/logout', logout)
 
 
 
