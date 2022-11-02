750149633198c0f6bd5eb5ff715eb4aa7d2b1ed2
geraldWaga92 <106008160+geraldWaga92@users.noreply.github.com>
2022-11-01 20:42:47 -0800

    create register function for our authentication and password hashing

---
+++ api/controllers/auth.js
@@ -0,0 +1,45 @@
+import { db } from "../connectMysql";
+import bcrypt from 'bcryptjs';
+
+export const register = (req, res) => {
+    //----check if user exist----
+
+    const q = 'SELECT * FROM users WHERE username = ?'
+
+    //the '[req.body.username]' is the value the username will hold when a user is registered for the login
+    db.query(q, [req.body.username], (err, data) => {
+        if(err) return res.status(500).json(err)
+        //this will check if there is already a user
+        if(data.length) return res.status(409).json('user already exist')
+
+        //----Create a user if no User found----
+        //Hash the password with bcryptjs
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(req.body.password, salt)
+
+        //this will be inserted in our Mysql database
+        const q = 'INSERT INTO users (`username`, `email`, `password`, `name` ) VALUE(?)'
+
+        //must be inorder from above to work
+        const values = [
+            req.body.username, 
+            req.body.email, 
+            hashedPassword, 
+            req.body.name
+        ]
+
+        db.query(q, [values], (err, data) => {
+            if(err) return res.status(500).json(err)
+            return res.status(200).json('user has been created')
+         })
+    })
+
+}
+
+export const login = (req, res) => {
+    
+}
+
+export const logout = (req, res) => {
+    
+}
