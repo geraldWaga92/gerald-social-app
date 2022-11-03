e284eda29d93689d1d239971c7a5be47429cfe52
geraldWaga92 <106008160+geraldWaga92@users.noreply.github.com>
2022-11-02 19:27:27 -0800

    get the userId on our post controller and set some condition on our mysql varialbe

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,18 +1,39 @@
 import { db } from '../connectMysql.js';
+import Jwt from 'jsonwebtoken';
 
 export const getPosts = (req, res) => {
 
-    //this line means that in our posts or what we see on our client side post is we have two components which is the users profile with it's name
-    //and the posts itself, let's say we want to target the users profile so first we combine the two component and then make a condition that
-    //the 'u.id' means the user id, and we place the 'u.id' inside the name userId together with name and profile. So this is a confusing line
-    //but this is all what is does, it's just assigning values
-    const q = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users
-    AS u ON (u.id = p.userId)`
+    //to get the users on the post we need the accessToken, remember the accessToken exist together with the logged in user, meaning the 
+    //current user logged in, so to get the user we write the code line below. This assume that the user is logged in
+    const token = req.cookies.accessToken;
 
+    //if the user is not logged in
+    if(!token) return res.status(401).json('Not logged in!')
 
-    db.query(q, (error, data) => {
-        if (error) return res.status(500).json(error);
-        return res.status(200).json(data);
+    //now if ther user is logged in and there is a token, we first verify if it is valid, so we compare the token, and 'secretkey'. if there
+    //is an error we throw token is not valid if is valid we return the data on this line "db.query(q, [data.id], (error, data)"
+    Jwt.verify(token, 'secretkey', (error, data) => {
+        if (error) return res.status(403).json('Token is not valid!');
+
+        //this line means that in our posts or what we see on our client side post is we have two components which is the users profile with it's name
+        //and the posts itself, let's say we want to target the users profile so first we combine the two component and then make a condition that
+        //the 'u.id' means the user id, and we place the 'u.id' inside the name userId together with name and profile. So this is a confusing line
+        //but this is all what is does, it's just assigning values
+        const q = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users
+        AS u ON (u.id = p.userId) JOIN relationships AS r ON (p.userId = r.followedUserId 
+        AND r.followerUserId = ?)`;
+    
+    
+        //then we use the db which connect from our database and use query method to search for a data, and then use our q variable as argument for 
+        //giving conditions, next is we get the data from the arguments above and remember that data we are refferring to is the data object from auth.js
+        //remember the object "id: data[0].id". So if we say data.id this is thesame as 'data[0].id', remeber that we are targeting the current user
+        //that is logged in and not the other user
+        db.query(q, [data.id], (error, data) => {
+            if (error) return res.status(500).json(error);
+            return res.status(200).json(data);
+        })
+
     })
+
 
 }
