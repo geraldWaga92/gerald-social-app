c4ea2d247b6a7517b65e357bb21b24dca5c13f42
geraldWaga92 <106008160+geraldWaga92@users.noreply.github.com>
2022-11-04 10:27:11 -0800

    update user with comment

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -7,6 +7,7 @@ export const getUser = (req, res) => {
 
   db.query(q, [userId], (err, data) => {
     if (err) return res.status(500).json(err);
+    //here we don't want our password to be displayed in the object
     const { password, ...info } = data[0];
     return res.json(info);
   });
