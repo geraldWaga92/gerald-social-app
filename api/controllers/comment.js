a6020b7c5b82923e57d93b13a9de2774966fbd4a
geraldWaga92 <106008160+geraldWaga92@users.noreply.github.com>
2022-11-03 16:26:47 -0800

    create addComment on our comment api

--- api/controllers/comment.js
+++ api/controllers/comment.js
@@ -16,3 +16,25 @@ export const getComments = (req, res) => {
     return res.status(200).json(data);
   });
 };
+
+export const addComment = (req, res) => {
+    const token = req.cookies.accessToken;
+    if (!token) return res.status(401).json("Not logged in!");
+  
+    jwt.verify(token, "secretkey", (err, userInfo) => {
+      if (err) return res.status(403).json("Token is not valid!");
+  
+      const q = "INSERT INTO comments(`desc`, `createdAt`, `userId`, `postId`) VALUES (?)";
+      const values = [
+        req.body.desc,
+        moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+        userInfo.id,
+        req.body.postId
+      ];
+  
+      db.query(q, [values], (err, data) => {
+        if (err) return res.status(500).json(err);
+        return res.status(200).json("Comment has been created.");
+      });
+    });
+  };
